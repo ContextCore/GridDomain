35684e11c30181c364aeb4dd76f087e9a97b7b76
andrey.leskov <andrey.leskov@solomoto.com>
2016-04-28 21:39:50 +0300

    New aggregate for saga usage demonstration

---
+++ GridDomain.Balance/Domain/ServiceSubscriptionAggregate/ServiceSubscription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Itenso.TimePeriod;
+
+namespace GridDomain.Balance.Domain.ServiceSubscriptionAggregate
+{
+    class ServiceSubscription
+    {
+        public TimeInterval Period;
+    }
+
+}
