0a7731ce880bcf13e8575a3acf74b9fd5d0c83d1
andrey.leskov <andrey.leskov@solomoto.com>
2016-05-25 19:30:01 +0300

    Added custom exception to AggregateCommandsHandler

--- GridDomain.CQRS.Messaging/MessageRouting/AggregateCommandsHandler.cs
+++ GridDomain.CQRS.Messaging/MessageRouting/AggregateCommandsHandler.cs
@@ -1,43 +1,62 @@
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using CommonDomain.Core;
 using GridDomain.EventSourcing;
 
 namespace GridDomain.CQRS.Messaging.MessageRouting
 {
-    public class AggregateCommandsHandler<T>: IAggregateCommandsHandler<T>,
-                                              ICommandAggregateLocator<T>
-        where T : AggregateBase
+    public class AggregateCommandsHandler<TAggregate>: IAggregateCommandsHandler<TAggregate>,
+                                              ICommandAggregateLocator<TAggregate>
+        where TAggregate : AggregateBase
     {
         public Guid GetAggregateId(ICommand command)
         {
             return GetHandler(command).GetId(command);
         }
 
-        private AggregateCommandHandler<T> GetHandler(ICommand cmd)
+        private AggregateCommandHandler<TAggregate> GetHandler(ICommand cmd)
         {
-            return _commandHandlers[cmd.GetType()];
+            AggregateCommandHandler<TAggregate> aggregateCommandHandler;//
+            var commandType = cmd.GetType();
+            if (!_commandHandlers.TryGetValue(commandType, out aggregateCommandHandler))
+                throw new CannotFindAggregateCommandHandlerExeption(typeof (TAggregate), commandType);
+                
+
+            return aggregateCommandHandler;
         }
 
-        private void Map(AggregateCommandHandler<T> handler)
+        private void Map(AggregateCommandHandler<TAggregate> handler)
         {
-            _commandHandlers[typeof (T)] = handler;
+            _commandHandlers[typeof (TAggregate)] = handler;
         }
 
-        protected void Map<TCommand>(Func<TCommand, Guid> idLocator, Action<TCommand, T> commandExecutor) where TCommand : ICommand
+        protected void Map<TCommand>(Func<TCommand, Guid> idLocator, Action<TCommand, TAggregate> commandExecutor) where TCommand : ICommand
         {
-            Map(AggregateCommandHandler<T>.New<TCommand>(idLocator, commandExecutor));
+            Map(AggregateCommandHandler<TAggregate>.New<TCommand>(idLocator, commandExecutor));
         }
 
-        protected void Map<TCommand>(Func<TCommand, Guid> idLocator, Func<TCommand, T> commandExecutor) where TCommand : ICommand
+        protected void Map<TCommand>(Func<TCommand, Guid> idLocator, Func<TCommand, TAggregate> commandExecutor) where TCommand : ICommand
         {
-            Map(AggregateCommandHandler<T>.New<TCommand>(idLocator, commandExecutor));
+            Map(AggregateCommandHandler<TAggregate>.New<TCommand>(idLocator, commandExecutor));
         }
 
-        private readonly IDictionary<Type, AggregateCommandHandler<T>> _commandHandlers = new Dictionary<Type, AggregateCommandHandler<T>>();
-        public IReadOnlyCollection<DomainEvent> Execute(T aggregate, ICommand command)
+        private readonly IDictionary<Type, AggregateCommandHandler<TAggregate>> _commandHandlers = new Dictionary<Type, AggregateCommandHandler<TAggregate>>();
+        public IReadOnlyCollection<DomainEvent> Execute(TAggregate aggregate, ICommand command)
         {
             return GetHandler(command).Execute(aggregate, command);
+        }
+    }
+
+    internal class CannotFindAggregateCommandHandlerExeption : Exception
+    {
+        public Type Type { get; }
+        public Type CommandType { get; }
+
+        public CannotFindAggregateCommandHandlerExeption(Type type, Type commandType)
+        {
+            Type = type;
+            CommandType = commandType;
         }
     }
 }
