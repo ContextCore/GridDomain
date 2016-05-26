0c752be53ea37c378cb2241818b96ac0637d3814
andrey.leskov <andrey.leskov@solomoto.com>
2016-05-26 18:07:09 +0300

    Changed serializer for actors to Wire from Newtonsoft

--- GridDomain.Node/ActorSystemFactory.cs
+++ GridDomain.Node/ActorSystemFactory.cs
@@ -20,91 +20,100 @@ namespace GridDomain.Node
         public static ActorSystem CreateActorSystem(AkkaConfiguration akkaConf)
         {
             var hoconConfig = @"akka {  
-                        actor {
-                                 provider = ""Akka.Cluster.ClusterActorRefProvider, Akka.Cluster""
-                                 loggers = [""Akka.Logger.NLog.NLogLogger, Akka.Logger.NLog""]
-                                 debug {
-                                          receive = on
-                                          autoreceive = on
-                                          lifecycle = on
-                                          event-stream = on
-                                          unhandled = on
-                                       }
-                        }
-                        stdout-loglevel = " + akkaConf.LogLevel + @"
-                        loglevel = " + akkaConf.LogLevel + @"
-                        log-config-on-start = on
+                                        stdout-loglevel = " + akkaConf.LogLevel + @"
+                                        loglevel = " + akkaConf.LogLevel + @"
+                                        log-config-on-start = on
 
-                        remote {
-                                    helios.tcp {
-                                        transport-class = ""Akka.Remote.Transport.Helios.HeliosTcpTransport, Akka.Remote""
-                                        transport-protocol = tcp
-                                        port = " + akkaConf.Network.PortNumber + @"}
-                                        hostname = " + akkaConf.Network.Name + @"/
+                                        actor {
+                                                serializers {
+                                                  wire = ""Akka.Serialization.WireSerializer, Akka.Serialization.Wire""
+                                                }
+                                                    serialization-bindings {
+                                                  ""System.Object"" = wire
+                                                }
+
+                                                 provider = ""Akka.Cluster.ClusterActorRefProvider, Akka.Cluster""
+                                                 loggers = [""Akka.Logger.NLog.NLogLogger, Akka.Logger.NLog""]
+                                                 debug {
+                                                          receive = on
+                                                          autoreceive = on
+                                                          lifecycle = on
+                                                          event-stream = on
+                                                          unhandled = on
+                                                       }
+                                        }
+                      
+
+                                        remote {
+                                                    helios.tcp {
+                                                        transport-class = ""Akka.Remote.Transport.Helios.HeliosTcpTransport, Akka.Remote""
+                                                        transport-protocol = tcp
+                                                        port = " + akkaConf.Network.PortNumber + @"}
+                                                        hostname = " + akkaConf.Network.Name + @"/
                                        
+                                                    }
+                                                }
+
+                                        persistence{
+
+                                                journal {
+                                                    sql-server {
+
+                                                        # qualified type name of the SQL Server persistence journal actor
+                                                        class = ""Akka.Persistence.SqlServer.Journal.SqlServerJournal, Akka.Persistence.SqlServer""
+
+                                                        # dispatcher used to drive journal actor
+                                                        plugin-dispatcher = ""akka.actor.default-dispatcher""
+
+                                                        # connection string used for database access
+                                                        connection-string = """+ akkaConf.Persistence.JournalConnectionString + @"""
+
+                                                        # default SQL commands timeout
+                                                        connection-timeout = 30s
+
+                                                        # SQL server schema name to table corresponding with persistent journal
+                                                        schema-name = dbo
+
+                                                        # SQL server table corresponding with persistent journal
+                                                        table-name = """ + akkaConf.Persistence.JournalTableName + @"""
+
+                                                        # should corresponding journal table be initialized automatically
+                                                        auto-initialize = off
+
+                                                        # timestamp provider used for generation of journal entries timestamps
+                                                        timestamp-provider = ""Akka.Persistence.Sql.Common.Journal.DefaultTimestampProvider, Akka.Persistence.Sql.Common""
+
+                                                        # metadata table
+                                                        metadata-table-name = """ + akkaConf.Persistence.MetadataTableName + @"""
+                                                    }
+                                                }
+
+                                                snapshot-store {
+                                                    sql-server {
+
+                                                        # qualified type name of the SQL Server persistence journal actor
+                                                        class = ""Akka.Persistence.SqlServer.Snapshot.SqlServerSnapshotStore, Akka.Persistence.SqlServer""
+
+                                                        # dispatcher used to drive journal actor
+                                                        plugin-dispatcher = ""akka.actor.default-dispatcher""
+
+                                                        # connection string used for database access
+                                                        connection-string = """ + akkaConf.Persistence.SnapshotConnectionString + @"""
+
+                                                        # default SQL commands timeout
+                                                        connection-timeout = 30s
+
+                                                        # SQL server schema name to table corresponding with persistent journal
+                                                        schema-name = dbo
+
+                                                        # SQL server table corresponding with persistent journal
+                                                        table-name = """ + akkaConf.Persistence.SnapshotTableName + @"""
+
+                                                        # should corresponding journal table be initialized automatically
+                                                        auto-initialize = off
+                                            }
+                                        }
                                     }
-                                }
-
-                        akka.persistence{
-
-                        journal {
-                            sql-server {
-
-                                # qualified type name of the SQL Server persistence journal actor
-                                class = ""Akka.Persistence.SqlServer.Journal.SqlServerJournal, Akka.Persistence.SqlServer""
-
-                                # dispatcher used to drive journal actor
-                                plugin-dispatcher = ""akka.actor.default-dispatcher""
-
-                                # connection string used for database access
-                                connection-string = """+ akkaConf.Persistence.JournalConnectionString + @"""
-
-                                # default SQL commands timeout
-                                connection-timeout = 30s
-
-                                # SQL server schema name to table corresponding with persistent journal
-                                schema-name = dbo
-
-                                # SQL server table corresponding with persistent journal
-                                table-name = """ + akkaConf.Persistence.JournalTableName + @"""
-
-                                # should corresponding journal table be initialized automatically
-                                auto-initialize = off
-
-                                # timestamp provider used for generation of journal entries timestamps
-                                timestamp-provider = ""Akka.Persistence.Sql.Common.Journal.DefaultTimestampProvider, Akka.Persistence.Sql.Common""
-
-                                # metadata table
-                                metadata-table-name = """ + akkaConf.Persistence.MetadataTableName + @"""
-                            }
-                        }
-
-                        snapshot-store {
-                            sql-server {
-
-                                # qualified type name of the SQL Server persistence journal actor
-                                class = ""Akka.Persistence.SqlServer.Snapshot.SqlServerSnapshotStore, Akka.Persistence.SqlServer""
-
-                                # dispatcher used to drive journal actor
-                                plugin-dispatcher = ""akka.actor.default-dispatcher""
-
-                                # connection string used for database access
-                                connection-string = """ + akkaConf.Persistence.SnapshotConnectionString + @"""
-
-                                # default SQL commands timeout
-                                connection-timeout = 30s
-
-                                # SQL server schema name to table corresponding with persistent journal
-                                schema-name = dbo
-
-                                # SQL server table corresponding with persistent journal
-                                table-name = """ + akkaConf.Persistence.SnapshotTableName + @"""
-
-                                # should corresponding journal table be initialized automatically
-                                auto-initialize = off
-                    }
-                        }
-                    }
 
                        ";
             var actorSystem = ActorSystem.Create(akkaConf.Network.Name, hoconConfig);
