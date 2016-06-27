14de128eb416355fccf0dbedc84eb5a46cf11501
Vadim Zhogalsky <vadim.zhogalsky@solomoto.com>
2016-06-27 11:15:09 +0300

    subscribtion change saga

--- GridDomain.Node/AkkaMessaging/Routing/ActorMessagesRouter.cs
+++ GridDomain.Node/AkkaMessaging/Routing/ActorMessagesRouter.cs
@@ -39,7 +39,7 @@ namespace GridDomain.Node.AkkaMessaging.Routing
                 c.Property
             )).ToArray();
 
-            var name = $"Aggregate_{typeof(TAggregate).FullName}";
+            var name = $"Aggregate_{typeof(TAggregate).Name}";
             var createActorRoute = CreateActorRouteMessage.ForAggregate<TAggregate>(name, messageRoutes);
             _routingActorTypedMessageActor.Handle(createActorRoute);
         }
