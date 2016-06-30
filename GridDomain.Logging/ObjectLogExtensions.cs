14d9f1ee6e29dcc5d8cdaa373244bc654c3fbbeb
andrey.leskov <andrey.leskov@solomoto.com>
2016-06-30 16:12:04 +0300

    fixed some wrong changes

--- GridDomain.Logging/ObjectLogExtensions.cs
+++ GridDomain.Logging/ObjectLogExtensions.cs
@@ -2,5 +2,18 @@
 
 namespace GridDomain.Logging
 {
- 
+    public static class ObjectLogExtensions
+    {
+        private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.All
+        };
+
+        public static string ToPropsString(this object o)
+        {
+            return JsonConvert.SerializeObject(o,
+                Formatting.Indented,
+                JsonSerializerSettings);
+        }
+    }
 }
