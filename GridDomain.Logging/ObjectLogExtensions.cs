fd7a5cd8ff3fff5ba703499329e47cdf80dc529f
andrey.leskov <andrey.leskov@solomoto.com>
2016-06-30 16:09:19 +0300

    Newtonsoft.JSon version 8.0.0 for all project, packages nuspecs

--- GridDomain.Logging/ObjectLogExtensions.cs
+++ GridDomain.Logging/ObjectLogExtensions.cs
@@ -2,18 +2,5 @@
 
 namespace GridDomain.Logging
 {
-    public static class ObjectLogExtensions
-    {
-        private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
-        {
-            TypeNameHandling = TypeNameHandling.All
-        };
-
-        public static string ToPropsString(this object o)
-        {
-            return JsonConvert.SerializeObject(o,
-                Formatting.Indented,
-                JsonSerializerSettings);
-        }
-    }
+ 
 }
