888b280b08ddeac21f24f270cc5d18fbd3654660
Vadim Zhogalsky <vadim.zhogalsky@solomoto.com>
2016-07-01 16:22:26 +0300

    build fixed

---
+++ GridDomain.Tests.Acceptance/DateTimeSpec.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading;
+using GridDomain.Common;
+using GridDomain.Tests.Framework;
+using NUnit.Framework;
+
+namespace GridDomain.Tests.Acceptance
+{
+    [TestFixture]
+    public class DateTimeSpec
+    {
+        [Test]
+        public void When_EditableDateTimeStrategy_is_used_and_Now_is_set_Then_DateTimeFacadeNow_should_be_the_same()
+        {
+            var testTime = DateTime.Now.AddSeconds(-3);
+            DateTimeStrategyHolder.Current = new TestDateTimeStrategy
+            {
+                EditableNow = testTime,
+            };
+            var now = DateTimeFacade.Now;
+            Assert.True(now == testTime);
+        }
+
+        [Test]
+        public void When_DefaultDateTimeStrategy_is_used_Then_DateTimeNow_should_differ_in_different_calls()
+        {
+            DateTimeStrategyHolder.Current = new DefaultDateTimeStrategy();
+            var firstTime = DateTimeFacade.Now;
+            Thread.Sleep(5);
+            var secondTime = DateTimeFacade.Now;
+            Assert.True(firstTime != secondTime);
+        }
+    }
+}
