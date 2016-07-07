ca91a685c7eef5bfc1eceade241d14774cac7694
andrey.leskov <andrey.leskov@solomoto.com>
2016-07-08 00:20:25 +0300

    forgoten file added and changed Quartz connection string to localDb

--- GridDomain.Scheduling/Quartz/PersistedQuartzConfig.cs
+++ GridDomain.Scheduling/Quartz/PersistedQuartzConfig.cs
@@ -4,7 +4,7 @@ namespace GridDomain.Scheduling.Quartz
 {
     public class PersistedQuartzConfig : IQuartzConfig
     {
-        public string ConnectionString => @"Data Source=(localdb);Initial Catalog=Quartz;Integrated Security = true";
+        public string ConnectionString => @"Data Source=(localdb)\v11.0;Initial Catalog=Quartz;Integrated Security = true";
         public string StorageType => "Quartz.Impl.AdoJobStore.JobStoreTX, Quartz";
 
         public NameValueCollection Settings => new NameValueCollection
