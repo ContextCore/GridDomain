d26188316ec0662134210c3361f3293be77d1551
Vadim Zhogalsky <vadim.zhogalsky@solomoto.com>
2016-07-13 11:21:11 +0300

    slack logging disabled

--- GridDomain.Logging/SerilogLogger.cs
+++ GridDomain.Logging/SerilogLogger.cs
@@ -11,7 +11,7 @@ namespace GridDomain.Logging
         static SerilogLogger()
         {
             Log =  new LoggerConfiguration().WriteTo.RollingFile("C:\\Logs\\logs-{Date}.txt")
-                                            .WriteTo.Slack("https://hooks.slack.com/services/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")
+                                            //.WriteTo.Slack("https://hooks.slack.com/services/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")
                                             .WriteTo.Elasticsearch("http://soloinfra.cloudapp.net:9222")
                                             .CreateLogger();
         }
