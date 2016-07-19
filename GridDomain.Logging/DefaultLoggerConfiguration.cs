b155a136b226541188f1852d7a53e3e13b563019
Vadim Zhogalsky <vadim.zhogalsky@solomoto.com>
2016-07-19 10:32:56 +0300

    fixed logging

--- GridDomain.Logging/DefaultLoggerConfiguration.cs
+++ GridDomain.Logging/DefaultLoggerConfiguration.cs
@@ -6,7 +6,11 @@ namespace GridDomain.Logging
     {
         public DefaultLoggerConfiguration()
         {
-            this.WriteTo.Console();
+            WriteTo.RollingFile(".\\GridDomainLogs\\logs-{Date}.txt").
+            //.WriteTo.Slack("https://hooks.slack.com/services/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")
+            WriteTo.Elasticsearch("http://soloinfra.cloudapp.net:9222")
+            .Enrich
+            .WithMachineName();
         }
     }
 }
