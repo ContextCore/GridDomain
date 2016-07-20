3043d86c012b6e8e13c2276d98d09510d135b6c6
vitaly.derbin <vitaliy.derbin@solomoto.com>
2016-07-20 18:16:25 +0300

    tests configuration fixes

--- GridDomain.Node/Configuration/Persistence/LocalDbConfiguration.cs
+++ GridDomain.Node/Configuration/Persistence/LocalDbConfiguration.cs
@@ -3,9 +3,9 @@ namespace GridDomain.Node.Configuration.Persistence
     public class LocalDbConfiguration : IDbConfiguration
     {
         public string ReadModelConnectionString
-            => @"Data Source=(local);Initial Catalog=AutoTestGridDomainRead;Integrated Security = true";
+            => @"Data Source=(localdb)\v11.0;Initial Catalog=AutoTestGridDomainRead;Integrated Security = true";
 
         public string LogsConnectionString
-            => @"Data Source=(local);Initial Catalog=AutoTestGridDomainLogs;Integrated Security = true";
+            => @"Data Source=(localdb)\v11.0;Initial Catalog=AutoTestGridDomainLogs;Integrated Security = true";
     }
 }
