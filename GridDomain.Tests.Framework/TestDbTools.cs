435604d9383bce6d07c74ec1f6469732780d4f60
andrey.leskov <andrey.leskov@solomoto.com>
2016-07-22 16:33:15 +0300

    deleting unnecessary code

--- GridDomain.Tests.Framework/TestDbTools.cs
+++ GridDomain.Tests.Framework/TestDbTools.cs
@@ -54,33 +54,6 @@ namespace GridDomain.Tests.Framework
                 }
             }
         }
-
-        //[Obsolete("Too slow")]
-        //public static void ClearDbByDelete(DbContext context)
-        //{
-        //    //TODO: понять, почему вызывает конструктор контекста по-умолчанию и как отучить 
-        //    ObjectContext objectContext = null;
-        //    try
-        //    {
-        //        context.Database.CreateIfNotExists();
-        //        objectContext = ((IObjectContextAdapter) context).ObjectContext;
-        //    }
-        //    catch (SqlException ex)
-        //    {
-        //        Console.WriteLine("DB clearing exception"); //shut up with damn CreatedOn
-        //    }
-        //    var tableNames = objectContext.MetadataWorkspace
-        //        .GetItemCollection(DataSpace.SSpace)
-        //        .GetItems<EntityContainer>()
-        //        .SelectMany(c => c.BaseEntitySets.Select(s => s.Table))
-        //        .Where(t => t != null);
-
-        //    foreach (var tableName in tableNames)
-        //    {
-        //        context.Database.ExecuteSqlCommand($"delete from {tableName}");
-        //    }
-        //}
-
         public static void RecreateWriteDb(string eventStoreConnectionString)
         {
             using (var conn = new SqlConnection(eventStoreConnectionString))
