ed8895984b73b3db1689a34532983e84cfab71f5
andrey.leskov <andrey.leskov@solomoto.com>
2016-08-24 13:13:54 +0300

    Rollback Wire to 0.0.6 due to Akka.Serialization.Wire is not compatible with newer Wire versions

--- GridDomain.CQRS.Messaging/IEventHandler.cs
+++ GridDomain.CQRS.Messaging/IEventHandler.cs
@@ -1,10 +1,12 @@
-﻿namespace GridDomain.CQRS.Messaging
+﻿using GridDomain.EventSourcing;
+
+namespace GridDomain.CQRS.Messaging
 {
     /// <summary>
     ///     Обработчик событый домена. Как правило, нужен для сбора модели чтения или оповещений.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public interface IEventHandler<in T> : IHandler<T>
+    public interface IEventHandler<in T> : IHandler<T> where T:DomainEvent
     {
     }
 }
