9b9b907bb88f84ae1106a36c59513728ec5d3d15
Sergey Zhurbenko <sergey.zhurbenko@solomoto.com>
2016-09-20 13:58:44 +0300

    SOLO-2365 added db configurations from configs

--- GridDomain.Node/Configuration/Persistence/LocalDbConfiguration.cs
+++ GridDomain.Node/Configuration/Persistence/LocalDbConfiguration.cs
@@ -1,11 +1,13 @@
+using System.Configuration;
+
 namespace GridDomain.Node.Configuration.Persistence
 {
     public class LocalDbConfiguration : IDbConfiguration
     {
         public string ReadModelConnectionString
-            => @"Server=tcp:soloinfra.cloudapp.net,5099;Database=sandboxMembershipReadAutoTests;User ID=solomoto;Password=s0l0moto;MultipleActiveResultSets=True";
+            => ConfigurationManager.ConnectionStrings["GridDomainReadTestString"].ConnectionString ?? @"Server=tcp:soloinfra.cloudapp.net,5099;Database=sandboxMembershipReadAutoTests;User ID=solomoto;Password=s0l0moto;MultipleActiveResultSets=True";
 
         public string LogsConnectionString
-            => @"Server=tcp:soloinfra.cloudapp.net,5099;Database=sandboxMembershipLogsAutoTests;User ID=solomoto;Password=s0l0moto;MultipleActiveResultSets=True";
+            => ConfigurationManager.ConnectionStrings["GridDomainLogsTestString"].ConnectionString ?? @"Server=tcp:soloinfra.cloudapp.net,5099;Database=sandboxMembershipLogsAutoTests;User ID=solomoto;Password=s0l0moto;MultipleActiveResultSets=True";
     }
 }
--- GridDomain.Tests.Framework/Configuration/AutoTestAkkaDbConfiguration.cs
+++ GridDomain.Tests.Framework/Configuration/AutoTestAkkaDbConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using GridDomain.Node.Configuration;
+﻿using System.Configuration;
+using GridDomain.Node.Configuration;
 using GridDomain.Node.Configuration.Akka;
 
 namespace GridDomain.Tests.Framework.Configuration
@@ -6,11 +7,10 @@ namespace GridDomain.Tests.Framework.Configuration
     public class AutoTestAkkaDbConfiguration : IAkkaDbConfiguration
     {
         public string SnapshotConnectionString
-            =>
-                "Server=tcp:soloinfra.cloudapp.net,5099;Database=sandboxMembershipWriteAutoTests;User ID=solomoto;Password=s0l0moto;MultipleActiveResultSets=True";
+            => ConfigurationManager.ConnectionStrings["GridDomainWriteTestString"].ConnectionString ?? "Server=tcp:soloinfra.cloudapp.net,5099;Database=sandboxMembershipWriteAutoTests;User ID=solomoto;Password=s0l0moto;MultipleActiveResultSets=True";
 
         public string JournalConnectionString
-            => "Server=tcp:soloinfra.cloudapp.net,5099;Database=sandboxMembershipWriteAutoTests;User ID=solomoto;Password=s0l0moto;MultipleActiveResultSets=True";
+            => ConfigurationManager.ConnectionStrings["GridDomainWriteTestString"].ConnectionString ?? "Server=tcp:soloinfra.cloudapp.net,5099;Database=sandboxMembershipWriteAutoTests;User ID=solomoto;Password=s0l0moto;MultipleActiveResultSets=True";
 
         public string MetadataTableName => "Metadata";
         public string JournalTableName => "Journal";
