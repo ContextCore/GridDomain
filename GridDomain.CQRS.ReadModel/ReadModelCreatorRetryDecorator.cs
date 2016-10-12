42b0eb17c3152142414651bf4338128d725fcc92
vitaly.derbin <vitaliy.derbin@solomoto.com>
2016-10-12 11:33:56 +0300

    Remove custom policies

--- GridDomain.CQRS.ReadModel/ReadModelCreatorRetryDecorator.cs
+++ GridDomain.CQRS.ReadModel/ReadModelCreatorRetryDecorator.cs
@@ -1,27 +1,26 @@
 using System;
 using System.Data.Entity.Infrastructure;
+using Polly;
 
 namespace GridDomain.CQRS.ReadModel
 {
     public class ReadModelCreatorRetryDecorator<T> : IReadModelCreator<T>
     {
         private readonly IReadModelCreator<T> _origCreator;
-        private readonly RetryPolicy<DbUpdateConcurrencyException> _retryPolicy;
 
         public ReadModelCreatorRetryDecorator(IReadModelCreator<T> origCreator)
         {
             _origCreator = origCreator;
-            _retryPolicy = RetryPolicy<DbUpdateConcurrencyException>.DefaultSql();
         }
 
         public void Add(T entity)
         {
-            _retryPolicy.Execute(() => _origCreator.Add(entity));
+            Policy.Handle<DbUpdateConcurrencyException>().Retry(5).Execute(() => _origCreator.Add(entity));
         }
 
         public void Modify(Guid id, Action<T> modificator)
         {
-            _retryPolicy.Execute(() => _origCreator.Modify(id, modificator));
+            Policy.Handle<DbUpdateConcurrencyException>().Retry(5).Execute(() => _origCreator.Modify(id, modificator));
         }
     }
 }
