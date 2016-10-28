1c3cfa302bbf53040d43490782d185cc9a6a4d6f
andrey.leskov <andrey.leskov@solomoto.com>
2016-10-28 17:44:52 +0300

    marking old expected messages functionality as obsolete

--- GridDomain.CQRS/ICommandExecutor.cs
+++ GridDomain.CQRS/ICommandExecutor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
@@ -6,7 +7,9 @@ namespace GridDomain.CQRS
     public interface ICommandExecutor
     {
         void Execute(params ICommand[] commands);
+        [Obsolete("Please use IMessageWaiterFactory.NewCommandWaiter instead")]
         Task<object> Execute(CommandPlan plan);
+        [Obsolete("Please use IMessageWaiterFactory.NewCommandWaiter instead")]
         Task<T> Execute<T>(CommandPlan<T> plan);
     }
 }
