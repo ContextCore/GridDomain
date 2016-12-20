47e9a70be94dff095f9834d99898914264a92c06
andrey.leskov <andrey.leskov@gmail.com>
2016-12-20 21:02:06 +0300

    added obsolete messages

--- GridDomain.CQRS/ICommandExecutor.cs
+++ GridDomain.CQRS/ICommandExecutor.cs
@@ -8,9 +8,9 @@ namespace GridDomain.CQRS
     public interface ICommandExecutor
     {
         void Execute(params ICommand[] commands);
-        [Obsolete("Please use IMessageWaiterFactory.NewCommandWaiter instead")]
+        [Obsolete("Please use ICommandWaiterFactory.PrepareCommand instead")]
         Task<object> Execute(CommandPlan plan);
-        [Obsolete("Please use IMessageWaiterFactory.NewCommandWaiter instead")]
+        [Obsolete("Please use ICommandWaiterFactory.PrepareCommand instead")]
         Task<T> Execute<T>(CommandPlan<T> plan);
         void Execute<T>(T command, IMessageMetadata metadata) where T : ICommand;
     }
--- GridDomain.CQRS/IMessageWaiterFactory.cs
+++ GridDomain.CQRS/IMessageWaiterFactory.cs
@@ -7,6 +7,8 @@ namespace GridDomain.CQRS
     public interface IMessageWaiterFactory
     {
         IMessageWaiter<Task<IWaitResults>> NewWaiter(TimeSpan? defaultTimeout = null);
+        
+        [Obsolete("Use ICommandWaiterFactory.PrepareCommand")]
         IMessageWaiter<IExpectedCommandExecutor> NewCommandWaiter(TimeSpan? defaultTimeout = null, bool failAnyFault = true);
     }
 
