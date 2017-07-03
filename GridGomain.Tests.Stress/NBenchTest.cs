f19d851759da6c28ab74ead435c5adf9dea5e7b6
andrey.leskov <andrey.leskov@solomoto.com>
2017-07-03 21:44:07 +0300

    added assembly binding for appveyour test

---
+++ GridGomain.Tests.Stress/NBenchTest.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+using NBench;
+using Pro.NBench.xUnit.XunitExtensions;
+using Xunit.Abstractions;
+
+namespace GridGomain.Tests.Stress {
+    public class NBenchTest
+    {
+        private Counter _counter;
+
+        public NBenchTest(ITestOutputHelper output)
+        {
+            Trace.Listeners.Clear();
+            Trace.Listeners.Add(new XunitTraceListener(output));
+        }
+
+        [PerfSetup]
+        public void Setup(BenchmarkContext context)
+        {
+            _counter = context.GetCounter("TotalCommandsExecutedCounter");
+        }
+
+        [NBenchFact]
+        [PerfBenchmark(Description = "Test to ensure that a minimal throughput test can be rapidly executed.",
+            NumberOfIterations = 5, RunMode = RunMode.Iterations,
+            RunTimeMilliseconds = 1000, TestMode = TestMode.Test)]
+        [CounterThroughputAssertion("TotalCommandsExecutedCounter", MustBe.GreaterThan, 400)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+        //MAX: 500
+        public void Benchmark()
+        {
+        }
+
+        [PerfCleanup]
+        public void Cleanup()
+        {
+        }
+    }
+}
