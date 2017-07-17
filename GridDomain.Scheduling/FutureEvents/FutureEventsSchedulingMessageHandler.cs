26d63f71dec604482ea1c56cf7b5a2b71d1bf9d5
andrey.leskov <andrey.leskov@solomoto.com>
2017-07-17 16:47:21 +0300

    configured quartz jobs fields to not exceed max column length in Sql database

--- GridDomain.Scheduling/FutureEvents/FutureEventsSchedulingMessageHandler.cs
+++ GridDomain.Scheduling/FutureEvents/FutureEventsSchedulingMessageHandler.cs
@@ -12,7 +12,7 @@ namespace GridDomain.Scheduling.FutureEvents
     ///     Listening to scheduling events from aggregates and modify quartz jobs accordinally
     /// </summary>
     internal class FutureEventsSchedulingMessageHandler : IHandler<FutureEventScheduledEvent>,
-                                                         IHandler<FutureEventCanceledEvent>
+                                                          IHandler<FutureEventCanceledEvent>
     {
         private readonly IActorRef _schedulerActorRef;
         private readonly ProcessEntry _schedulingFutureEventProcessEntry;
@@ -21,10 +21,10 @@ namespace GridDomain.Scheduling.FutureEvents
         {
             _schedulerActorRef = schedulingActor;
 
-            _schedulingFutureEventProcessEntry = new ProcessEntry(GetType().
-                    Name,
-                "Scheduling raise future event command",
-                "FutureEventScheduled event occured");
+            _schedulingFutureEventProcessEntry = new ProcessEntry(GetType()
+                                                                      .Name,
+                                                                  "Scheduling raise future event command",
+                                                                  "FutureEventScheduled event occured");
         }
 
         public Task Handle(FutureEventCanceledEvent evt, IMessageMetadata metadata)
@@ -38,28 +38,26 @@ namespace GridDomain.Scheduling.FutureEvents
             var scheduleId = message.Id;
             var succesEventType = message.Event.GetType();
 
-            var description = $"Source {message.SourceName} scheduled future event "
-                              + $"{scheduleId} with payload type {succesEventType. Name} on time {message.RaiseTime}\r\n"
-                              + $"Future event: {message.ToPropsString()}";
+            var description = $"Source {message.SourceName} id={message.SourceId} scheduled future event {message.Event.GetType().Name} id={scheduleId} o n{message.RaiseTime}\r\n";
 
             var scheduleKey = CreateScheduleKey(scheduleId, message.SourceId, message.SourceName, description);
 
             var command = new RaiseScheduledDomainEventCommand(message.Id, message.SourceId, Guid.NewGuid());
-            var metadata = messageMetadata.CreateChild(command.Id,_schedulingFutureEventProcessEntry);
+            var metadata = messageMetadata.CreateChild(command.Id, _schedulingFutureEventProcessEntry);
 
             var scheduleEvent = new ScheduleCommandExecution(command,
-                                                    scheduleKey,
-                                                    ExecutionOptions.ForCommand(message.RaiseTime, succesEventType),
-                                                    metadata);
+                                                             scheduleKey,
+                                                             ExecutionOptions.ForCommand(message.RaiseTime, succesEventType),
+                                                             metadata);
 
             return _schedulerActorRef.Ask<Scheduled>(scheduleEvent);
         }
 
-        internal static ScheduleKey CreateScheduleKey(Guid scheduleId, Guid sourceId, string sourceName, string description=null)
+        internal static ScheduleKey CreateScheduleKey(Guid scheduleId, Guid sourceId, string sourceName, string description = null)
         {
-            return new ScheduleKey($"{sourceName}_{sourceId:N}_fe_{scheduleId:N}",
-                $"{sourceName}_{sourceId:N}_future_events",
-                description);
+            return new ScheduleKey($"{scheduleId:N}",
+                                   $"{sourceName}_{sourceId:N}_future_events",
+                                   description);
         }
     }
 }
