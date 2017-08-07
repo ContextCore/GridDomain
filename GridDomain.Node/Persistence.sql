9232cf31dc042de23725353ebba95599bd8d9b72
andrey.leskov <andrey.leskov@gmail.com>
2017-08-07 14:06:16 +0300

    Added sql schema for db initialization

---
+++ GridDomain.Node/Persistence.sql
@@ -0,0 +1,28 @@
+﻿
+CREATE TABLE Journal (
+  Ordering BIGINT IDENTITY(1,1) NOT NULL,
+  PersistenceID NVARCHAR(255) NOT NULL,
+  SequenceNr BIGINT NOT NULL,
+  Timestamp BIGINT NOT NULL,
+  IsDeleted BIT NOT NULL,
+  Manifest NVARCHAR(500) NOT NULL,
+  Payload VARBINARY(MAX) NOT NULL,
+  Tags NVARCHAR(100) NULL
+	CONSTRAINT PK_Journal PRIMARY KEY (Ordering),
+  CONSTRAINT QU_Journal UNIQUE (PersistenceID, SequenceNr)
+);
+
+CREATE TABLE Snapshots (
+  PersistenceID NVARCHAR(255) NOT NULL,
+  SequenceNr BIGINT NOT NULL,
+  Timestamp DATETIME2 NOT NULL,
+  Manifest NVARCHAR(500) NOT NULL,
+  Snapshot VARBINARY(MAX) NOT NULL
+  CONSTRAINT PK_Snapshots PRIMARY KEY (PersistenceID, SequenceNr)
+);
+
+CREATE TABLE Metadata (
+  PersistenceID NVARCHAR(255) NOT NULL,
+  SequenceNr BIGINT NOT NULL,
+  CONSTRAINT PK_Metadata PRIMARY KEY (PersistenceID, SequenceNr)
+);
