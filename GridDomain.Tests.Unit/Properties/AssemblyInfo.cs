70b880eb7dedf938d99c61aff32deba70ea929eb
andrey.leskov <andrey.leskov@solomoto.com>
2017-08-18 20:58:53 +0300

    Restored GridDomain.Tests.Unit

--- GridDomain.Tests.Unit/Properties/AssemblyInfo.cs
+++ GridDomain.Tests.Unit/Properties/AssemblyInfo.cs
@@ -1,45 +1,6 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 using Xunit;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("GridDomain.Tests.XUnit")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GridDomain.Tests.XUnit")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("46cc4be2-93be-4807-9b1a-7a10dfea29af")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("GridDomain.Tests.Acceptance")]
 [assembly: InternalsVisibleTo("GridDomain.Tests.Acceptance")]
 #if !DEBUG
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
