a032afd86fe1fb268c805dd9b3809c4e498cbc70
andrey.leskov <andrey.leskov@solomoto.com>
2017-09-11 18:33:52 +0300

    Added assembly version attribute to projects and modified script for it

--- Nuget/set_version_for_nuget_dependencies.ps1
+++ Nuget/set_version_for_nuget_dependencies.ps1
@@ -4,12 +4,13 @@
 
 filter sed($before,$after) { %{$_ -replace $before,$after} } 
 
-function replace ($source,$dest,$pattern) {
-    Write-Host $dest
-    $configs = Get-ChildItem ..\ -Recurse *.nuspec
+function replace ($source,$dest,$pattern,$filesRegExp) {
+    Write-Host Replacing $source to $dest in files like $filesRegExp and containing $pattern
+    $configs = Get-ChildItem ..\ -Recurse $filesRegExp
     foreach ($config in $configs) {
         $match = cat $config.FullName | grep $pattern
-        if ($match -match $source ) { Write-Host "`nFound"$config.FullName
+        if ($match -match $source ) { 
+		    Write-Host "`nFound"$config.FullName
             Write-Host "`nReplace"$match
             $result = cat $config.FullName | sed -before $source -after $dest
             $result | Out-File $config.FullName -Encoding utf8
