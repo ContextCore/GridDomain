63dbaf76d88ac92ad1be286a8cadbbe0814fc794
andrey.leskov <andrey.leskov@gmail.com>
2017-09-23 10:35:18 +0300

    Created default config for akka db write part

--- GridDomain.Tests.Common/Configuration/AutoTestAkkaDbConfiguration.cs
+++ GridDomain.Tests.Common/Configuration/AutoTestAkkaDbConfiguration.cs
@@ -4,23 +4,18 @@ using GridDomain.Node.Configuration.Akka;
 namespace GridDomain.Tests.Common.Configuration
 {
 
-    public class AutoTestAkkaDbConfiguration : IAkkaDbConfiguration
+    public class AutoTestAkkaDbConfiguration : DefaultAkkaDbConfiguration
     {
+        public AutoTestAkkaDbConfiguration():base("Server=(local); Database = AutoTestWrite; Integrated Security = true; MultipleActiveResultSets = True")
+        {
+            
+        }
         private const string JournalConnectionStringName = "WriteModel";
         //enviroment variables - for appveour tests launch
-        public string SnapshotConnectionString
-            =>
-                Environment.GetEnvironmentVariable(JournalConnectionStringName) ?? "Server=(local); Database = AutoTestWrite; Integrated Security = true; MultipleActiveResultSets = True";
+        public override string SnapshotConnectionString
+            => Environment.GetEnvironmentVariable(JournalConnectionStringName) ?? base.SnapshotConnectionString;
 
-        public string JournalConnectionString
-            =>
-                 Environment.GetEnvironmentVariable(JournalConnectionStringName) ?? "Server=(local); Database = AutoTestWrite; Integrated Security = true; MultipleActiveResultSets = True";
-
-        public string MetadataTableName => "Metadata";
-        public string JournalTableName => "Journal";
-        public int JornalConnectionTimeoutSeconds => 120;
-        public int SnapshotsConnectionTimeoutSeconds => 120;
-        public string SnapshotTableName => "Snapshots";
-        public string SchemaName { get; } = "dbo";
-    }
+        public override string JournalConnectionString
+            => Environment.GetEnvironmentVariable(JournalConnectionStringName) ?? base.SnapshotConnectionString;
+        }
 }
