ab053acceb521a87da601ad9547a0aa4357cd5e7
Andrey Leskov <andrey.leskov@gmail.com>
2017-12-12 02:08:14 +0300

    moved unit tests to net core platform

--- GridDomain.Tests.Unit/ProcessManagers/ProcessManagerActorTests/Actors_should_fill_process_id_for_produced_faults.cs
+++ GridDomain.Tests.Unit/ProcessManagers/ProcessManagerActorTests/Actors_should_fill_process_id_for_produced_faults.cs
@@ -64,7 +64,7 @@ namespace GridDomain.Tests.Unit.ProcessManagers.ProcessManagerActorTests
             transport.Subscribe<MessageMetadataEnvelop<Fault<GoSleepCommand>>>(TestActor);
             var handlersActor = Sys.ActorOf(Props.Create(() => new HandlersPipeActor(new HandlersDefaultProcessor(), TestActor)));
 
-            var actor = Sys.ActorOf(Props.Create(() => new AggregateActor<ProgrammerAggregate>(CommandAggregateHandler.New<ProgrammerAggregate>(),
+            var actor = Sys.ActorOf(Props.Create(() => new AggregateActor<ProgrammerAggregate>(CommandAggregateHandler.New<ProgrammerAggregate>(null),
                                                                                          new SnapshotsPersistencePolicy(1, 5, null, null),
                                                                                          new AggregateFactory(),
                                                                                          handlersActor)),
