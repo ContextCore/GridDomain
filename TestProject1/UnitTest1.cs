1829bd5e084e31bf205e61b2d1f3a1501f1f12d1
Andrey Leskov <andrey.leskov@gmail.com>
2018-02-13 01:46:38 +0800

    Adding referenece project to debug nbech launch from xunit

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Pro.NBench.xUnit.XunitExtensions;
+using Xunit;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [NBenchFact]
+        public void Test1()
+        {
+            
+            
+        }
+    }
+}
