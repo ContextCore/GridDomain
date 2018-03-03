a1617d27efde531ec2b251cf7c63b8d51167d751
Andrey Leskov <andrey.leskov@gmail.com>
2018-03-03 17:38:16 +0800

    revert new changes to quartz config related to persistence

--- GridDomain.Scheduling/Quartz/Configuration/PersistedQuartzConfig.cs
+++ GridDomain.Scheduling/Quartz/Configuration/PersistedQuartzConfig.cs
@@ -21,7 +21,7 @@ namespace GridDomain.Scheduling.Quartz.Configuration
                     ["quartz.jobStore.clustered"] = "false",
                     ["quartz.jobStore.dataSource"] = "default",
                     ["quartz.jobStore.tablePrefix"] = "QRTZ_",
-                    ["quartz.jobStore.useProperties"] = "true",
+                  //  ["quartz.jobStore.useProperties"] = "true",
                     ["quartz.jobStore.lockHandler.type"] =
                     "Quartz.Impl.AdoJobStore.UpdateLockRowSemaphore, Quartz",
                     ["quartz.dataSource.default.connectionString"] = ConnectionString,
