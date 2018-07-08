4720e156047ee01f1b09fb1bf71d9fbf372242a4
Andrey Leskov <andrey.leskov@gmail.com>
2018-07-09 00:24:59 +0800

    triggering appveyor

--- GridDomain.Node/GridDomainNode.cs
+++ GridDomain.Node/GridDomainNode.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.DI.AutoFac;
