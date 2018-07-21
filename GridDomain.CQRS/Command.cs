b4b5c58a2fcfd50cb347a27edac674c55d0783a2
Andrey Leskov <andrey.leskov@gmail.com>
2018-07-21 23:00:26 +0800

    Moved command<T> class to GridDomain.CQRS library

--- GridDomain.CQRS/Command.cs
+++ GridDomain.CQRS/Command.cs
@@ -37,4 +37,13 @@ namespace GridDomain.CQRS
             return copy;
         }
     }
+
+    public class Command<T> : Command where T: class 
+    {
+        protected Command(string id, string aggregateId, string processId, DateTime time) : base(id, aggregateId, processId, time,typeof(T).BeautyName()) { }
+        protected Command(string id, string aggregateId, string processId) : base(id, aggregateId, processId,typeof(T).BeautyName()) { }
+        protected Command(string id, string aggregateId, DateTime time) : base(id, aggregateId, time,typeof(T).BeautyName()) { }
+        protected Command(string id, string aggregateId) : base(id, aggregateId, typeof(T).BeautyName()) { }
+        protected Command(string aggregateId) : base(aggregateId, typeof(T).BeautyName()) { }
+    }
 }
