99bc628dd3d6274fcbdddae097cc45665abb6080
Andrey Leskov <andrey.leskov@gmail.com>
2018-08-13 00:49:55 +0800

    Added new constraint to aggregate handler to avoid errors when aggregate handler has some constructor parameters

--- GridDomain.EventSourcing/CommandAggregateHandler.cs
+++ GridDomain.EventSourcing/CommandAggregateHandler.cs
@@ -4,7 +4,6 @@ using GridDomain.EventSourcing.CommonDomain;
 namespace GridDomain.EventSourcing {
     public static class CommandAggregateHandler
     {
-        
         public static IAggregateCommandsHandler<T> New<T>(IConstructAggregates factory = null) where T : CommandAggregate
         {
             return new ConventionAggregateHandler<T>((T)(factory ?? AggregateFactory.Default).Build(typeof(T),null,null));
