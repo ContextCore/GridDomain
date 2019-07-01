136d7a6b940c54cd677cbc7777b57663a3e0a5af
Andrey Leskov <andrey.leskov@gmail.com>
2019-07-01 12:40:46 +0800

    Move source name class to own file

---
+++ src/GridDomain.EventHandlers.Akka/SourceName.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+using GridDomain.Common;
+
+namespace GridDomain.EventHandlers.Akka
+{
+    public static class SourceName
+    {
+        public static string Get<TMessage>(this Type handlerType)
+        {
+            return Get(handlerType, typeof(TMessage));
+        }
+        
+        public static string Get<TMessageA,TMessageB>(this Type handlerType)
+        {
+            return Get(handlerType, typeof(TMessageA), typeof(TMessageB));
+        }
+
+        
+        public static string Get<THandler,TMessage>()
+        {
+            return Get(typeof(THandler), typeof(TMessage));
+        }
+        public static string Get<THandler,TMessageA,TMessageB>()
+        {
+            return Get(typeof(THandler), typeof(TMessageA),typeof(TMessageB));
+        }
+        
+        public static string Get(params Type[] types)
+        {
+            return string.Join("_", types.Select(t => TypeNameExtensions.BeautyName(t)));
+        }
+    }
+}
